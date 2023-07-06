13e64498bba09e4678536c6e90387ee915cc7d16
BenBernhardG <138679020+BenBernhardG@users.noreply.github.com>
2023-07-06 15:03:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BenBernhardG</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+</body>
+</html>
